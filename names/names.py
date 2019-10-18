2ee8f93f6e4a0466f7f9c4128ff8ca8c0beed3f2
Mindy Allen <mrsmallen07@gmail.com>
2019-10-18 13:44:41 -0400

    made changes to see if I can get the ruun time down under 10 seconds - tested -under 10 seconds

--- names/names.py
+++ names/names.py
@@ -3,11 +3,11 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = f.read().split("\100")  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = f.read().split("\100")  # List containing 10000 names
 f.close()
 
 duplicates = []
@@ -17,8 +17,8 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"{len(duplicates)} duplicates:\100\100{', '.join(duplicates)}\100\100")
 print (f"runtime: {end_time - start_time} seconds")
 
 #First test-  runtime: 10.456552982330322 seconds
-
+#Second test - runtime: 0.0015401840209960938 seconds 
